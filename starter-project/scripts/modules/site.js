8a28bd03a52ae9e67b946c7f87dbae1affdadb93
Gediminas Stanaitis <gedas@devbridge.com>
2014-12-09 13:40:49 +0200

    transfer original files

---
+++ starter-project/scripts/modules/site.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+/*jslint regexp: true, nomen: true, sloppy: true */
+/*global require, define, alert, applicationConfig, location, document, window,  setTimeout, Countable */
+
+define(['jquery'], function ($) {
+
+    var module = {};
+
+    module.sample = function () {
+        console.log('sample');
+    };
+
+    module.init = function () {
+        module.sample();
+    };
+
+    return module;
+});
