a9985932251bb2153af8298ed6fe1c757a4d22c4
VaclovasV <vaclovas@devbridge.com>
2018-02-20 10:29:07 +0200

    Update watch-svg.js

--- gulp-tasks/watch-svg.js
+++ gulp-tasks/watch-svg.js
@@ -2,7 +2,5 @@ module.exports = function () {
 	var gulp = require('gulp'),
 		config = require('../gulp.config.js')();
 
-	return gulp.watch(config.svg.sourceFolder + '**/*.svg', function () {
-		return gulp.watch(config.svg.sourceFolder + '**/*.svg', ['create-svg-sprite']);
-	});
+	return gulp.watch(config.svg.sourceFolder + '**/*.svg', ['create-svg-sprite']);
 };
