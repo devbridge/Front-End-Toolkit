eb2e5fc1723d402c1646dcfa7c25cff99e40c932
Vaclovas <vaclovas@devbridge.com>
2018-03-15 17:53:10 +0200

    Removed html folder from dist folder

--- gulp-tasks/html-render.js
+++ gulp-tasks/html-render.js
@@ -1,9 +1,11 @@
 module.exports = function () {
     let gulp = require('gulp'),
         htmlrender = require('gulp-htmlrender'),
+        rename = require('gulp-rename'),
         config = require('../gulp.config.js')();
 
     return gulp.src(config.html.src, {read: false})
         .pipe(htmlrender.render())
+        .pipe(rename({dirname: ''}))
         .pipe(gulp.dest('dist'));
 };
