9a3e59583ab48bf586ac43d7f192986bcfb4fd5a
Vaclovas <vaclovas@devbridge.com>
2018-03-15 18:12:47 +0200

    Flattened html structure

--- gulp-tasks/html-render.js
+++ gulp-tasks/html-render.js
@@ -1,11 +1,11 @@
 module.exports = function () {
     let gulp = require('gulp'),
         htmlrender = require('gulp-htmlrender'),
-        rename = require('gulp-rename'),
+        flatten = require('gulp-flatten'),
         config = require('../gulp.config.js')();
 
     return gulp.src(config.html.src, {read: false})
         .pipe(htmlrender.render())
-        .pipe(rename({dirname: ''}))
+        .pipe(flatten())
         .pipe(gulp.dest('dist'));
 };
